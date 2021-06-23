c54057d0726af889174ee8111e1ff0a522585d8b
Abbta <67863193+Abbta@users.noreply.github.com>
2021-06-23 10:33:34 +0200

    actions can now schedule

--- GameEngineTry5/OnActionBase.cpp
+++ GameEngineTry5/OnActionBase.cpp
@@ -10,5 +10,10 @@ namespace EngineName
 		{
 			mp_taskRef = mp_worldRef.mpc_theQueue.mpc_taskContainer.mpf_add<TaskType>(task);
 		}
+
+		void OnActionBase::mpf_schedule(Base::ObjectContainer& world, Time::Task& task)
+		{
+			world.mpc_theQueue.mpf_addToQueue(task);
+		}
 	}
 }
