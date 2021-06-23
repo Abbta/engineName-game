7a9707e65184bfa475d359d6f52bf6062f880967
Abbta <67863193+Abbta@users.noreply.github.com>
2021-06-23 10:37:53 +0200

    final implementation of actionlistener, with bugfixing it should work

--- GameEngineTry5/ActionListener.cpp
+++ GameEngineTry5/ActionListener.cpp
@@ -7,14 +7,18 @@ namespace EngineName
 	{
 		void ActionListener::click(const unsigned int x, const unsigned int y)
 		{
-			Object::Vector point(x, y);
 			std::list<std::unique_ptr<OnClick>>::iterator it;
 			it = mparr_clickListeners.begin();
 			while (it != mparr_clickListeners.end())
 			{
 				if ((*(*it)).onClick(x, y))
 				{
-					todo
+					//click was on button
+					//only one button can be pressed
+					//as they should be sorted from front to back visually
+					//this is the only button that can be clicked
+					//therefore function is terminated
+					return;
 				}
 				it++;
 			}
