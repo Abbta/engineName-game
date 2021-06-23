a75093c6eb39fad15f9d4d1a42a735d87053928e
Abbta <67863193+Abbta@users.noreply.github.com>
2021-06-23 10:30:58 +0200

    onClick function

--- GameEngineTry5/OnActionBase.h
+++ GameEngineTry5/OnActionBase.h
@@ -13,6 +13,8 @@ namespace EngineName
 		protected:
 			Time::Task& mp_taskRef;
 			Base::ObjectContainer& mp_worldRef;
+
+			void mpf_schedule(Base::ObjectContainer& world, Time::Task& task);
 		public:
 			template<class TaskType>
 			OnActionBase(Base::ObjectContainer& world, const TaskType& task);
--- GameEngineTry5/OnClick.cpp
+++ GameEngineTry5/OnClick.cpp
@@ -1,1 +1,16 @@
 #include "OnClick.h"
+namespace EngineName
+{
+	namespace Action
+	{
+		bool OnClick::onClick(const unsigned int x, const unsigned int y)
+		{
+			if (mp_areaRef.boxContains(Object::Vector(x, y)))
+			{
+				mpf_schedule(mp_worldRef, mp_taskRef);
+				return true;
+			}
+			return false;
+		}
+	}
+}
