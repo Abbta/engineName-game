5459dc8580ffb1666c5f0d5dbd50b22be02fb90c
Abbta <67863193+Abbta@users.noreply.github.com>
2021-11-09 17:04:43 +0100

    adapted newly merged button to main menu

--- GameEngineTry5/EngineNameMain.cpp
+++ GameEngineTry5/EngineNameMain.cpp
@@ -22,18 +22,16 @@ int engineNameMain(EngineName::Base::ObjectContainer &world)
 	//register all objects
 
 	world.backgroundColor = Color("AAAAFF");
-	world.rectangle.build(world, Vector((world.width * (3 / 8.0)), (world.height * (1/6.0))), world.width * (2/8.0), world.height * (4/7.0), Color("333355"));
+	auto menuRect = &world.rectangle.build(world, Vector((world.width * (3 / 8.0)), (world.height * (1/6.0))), world.width * (2/8.0), world.height * (4/7.0), Color("333355"));
 	const EngineName::Object::Rectangle buttonRectangle(
 		world,
 		Vector(world.width * (7 / 16.0), world.height * (3 / 12.0)),
 		world.width * (5 / 16.0),
 		world.height * (1 / 10.0),
 		Color("AA5678"));
-	EngineName::Object::Visible tempRef(buttonRectangle);
-	const EngineName::Time::Move buttonAction(tempRef, Vector(50, 0));
+	const EngineName::Time::Move buttonAction(*menuRect, Vector(0, 0)); //temporary null action
 	EngineName::Object::Button tempButton(buttonRectangle, buttonAction);
 	auto button = &world.button.build(tempButton);
-	button->changeOnClickTask(EngineName::Time::Move(*button, Vector(50, 0)));
 
 	return world.size();
 }
