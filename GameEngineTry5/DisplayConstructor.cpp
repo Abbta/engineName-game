2e11259dd74dd16e9ac1d2145bf45442ed6b5a88
Abbta <67863193+Abbta@users.noreply.github.com>
2022-02-03 21:54:36 +0100

    Bugfix: Display not assigned paintingorder correctly due to not being added to allVisibles at construction

--- GameEngineTry5/DisplayConstructor.cpp
+++ GameEngineTry5/DisplayConstructor.cpp
@@ -5,7 +5,9 @@ namespace EngineName
 	{
 		Display<int>& DisplayConstructor::build(const Display<int>& display)
 		{
+			this->mf_makeRoomForNewVisible();
 			mparr_intDisplays.push_back(std::make_unique<Display<int>>(display));
+			this->mf_registerNewVisible(*mparr_intDisplays.back());
 			return *mparr_intDisplays.back();
 		}
 	}
