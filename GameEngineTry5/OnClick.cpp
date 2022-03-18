9a19ba560018f5729e8a129732d494dc8dc38706
Abbta <67863193+Abbta@users.noreply.github.com>
2022-03-18 14:56:27 +0100

    minor

--- GameEngineTry5/OnClick.cpp
+++ GameEngineTry5/OnClick.cpp
@@ -1,10 +1,12 @@
 #include "OnClick.h"
+#include "ObjectContainer.h"
 namespace EngineName
 {
 	namespace Action
 	{
 		bool OnClick::onClick(const unsigned int x, const unsigned int y)
 		{
+			mp_worldRef.
 			if (mp_areaRef.boxContains(Object::Vector(x, y)))
 			{
 				mpf_schedule(mp_worldRef, mp_taskRef);
