fe14ecdd3b0a9862ef6d01e4dad9fe610246b4ab
Abbta <67863193+Abbta@users.noreply.github.com>
2022-03-21 18:38:07 +0100

    made OnClick check for active scene

--- GameEngineTry5/OnClick.cpp
+++ GameEngineTry5/OnClick.cpp
@@ -1,14 +1,20 @@
 #include "OnClick.h"
-#include "ObjectContainer.h"
+#include "ObjectContainerAccess.h"
 namespace EngineName
 {
 	namespace Action
 	{
 		bool OnClick::onClick(const unsigned int x, const unsigned int y)
 		{
-			mp_worldRef.
-			if (mp_areaRef.boxContains(Object::Vector(x, y)))
+			auto activeScene = Base::ObjectContainerAccess::getActiveScene(mp_worldRef);
+			if (activeScene)
+				if (!activeScene->contains(*mpptr_area))
+					//if there is an active scen but it doesnt contain are
+					return false;
+				//if the area is on screen
+			if ((*mpptr_area).boxContains(Object::Vector(x, y)))
 			{
+				//and click is within it
 				mpf_schedule(mp_worldRef, *mpptr_task);
 				return true;
 			}
