972fcba3b846cf9967431d80c330ec56852203ea
Abbta <67863193+Abbta@users.noreply.github.com>
2022-03-25 13:20:00 +0100

    ATButton cleaned

--- GameEngineTry5/ActivatableTButton.cpp
+++ GameEngineTry5/ActivatableTButton.cpp
@@ -3,6 +3,11 @@ namespace EngineName
 {
 	namespace Object
 	{
+		/* ATButton::changeTextRectangleBaseTo()
+		*	changes TextRectangle part of class
+		*	takes temporary TextRectangle as param
+		*	returns *this
+		*/
 		ActivatableTButton& ActivatableTButton::mpf_changeTRBaseTo(const TextRectangle& textRectangle)
 		{
 			//change Visible components
@@ -24,24 +29,29 @@ namespace EngineName
 			return *this;
 		}
 
+		//sets mode to active
 		ActivatableTButton& ActivatableTButton::setActive()
 		{
 			mpf_changeTRBaseTo(*mpptr_activeTRect.get());
+			//mp_toggle in actionRef is used to determine what mode class is in
 			mp_actionRef.mp_toggle = true;
 			//make it check for correct area in clicklistener
 			mpf_changeOnClickArea(*mpptr_activeTRect);
 			return *this;
 		}
 
+		//sets mode to inactive
 		ActivatableTButton& ActivatableTButton::setInactive()
 		{
 			mpf_changeTRBaseTo(*mpptr_inactiveTRect.get());
+			//mp_toggle in actionRef is used to determine what mode class is in
 			mp_actionRef.mp_toggle = false;
 			//make it check for correct area in clicklistener
 			mpf_changeOnClickArea(*mpptr_inactiveTRect);
 			return *this;
 		}
 
+		//sets mode to whatever it is not at the moment
 		ActivatableTButton& ActivatableTButton::toggleActive()
 		{
 			if (mp_actionRef.mp_toggle)
--- GameEngineTry5/ActivatableTButton.h
+++ GameEngineTry5/ActivatableTButton.h
@@ -4,36 +4,46 @@ namespace EngineName
 {
     namespace Object
     {
-        class ActivatableTButtonConstructor;
+        /* ActivatableTextButton class
+        *   Textbutton specialised to become activated and deactivated via task
+        *   Contains two textRectangles, one for active mode and one for inactive
+        *   Inactive mode also disables the OnClick for the button
+        */
         class ActivatableTButton :
             public TextButton
         {
         protected:
+            //Two textrectangles being the frames for active and inactive mode are stored in the class
             std::unique_ptr<TextRectangle> mpptr_activeTRect;
             std::unique_ptr<TextRectangle> mpptr_inactiveTRect;
 
+            //Method that actually changes the Textrectangle part of class to the desired TR frame
             ActivatableTButton& mpf_changeTRBaseTo(const TextRectangle& textRectangle);
         public:
+            //note how constructor is templated due to onClickTask being any task type
             template<class t_TaskType>
             ActivatableTButton(const TextRectangle& activeTRect, const TextRectangle& inactiveTRect, const t_TaskType& onClickTask, const bool isActive = false)
                 : mpptr_activeTRect(std::make_unique<TextRectangle>(activeTRect)), mpptr_inactiveTRect(std::make_unique<TextRectangle>(inactiveTRect)),
                 TextButton(inactiveTRect, onClickTask), Rectangle(inactiveTRect)
             {
+                //toggle the OnClick
                 mp_actionRef.mp_toggle = isActive;
+                //the toggle in OnClick is used to determine what mode ATButton is in
                 if (mp_actionRef.mp_toggle)
                     setActive();
             }
 
+            //default constructor not templated
             ActivatableTButton(const ActivatableTButton& activatableTButton)
                 :mpptr_activeTRect(std::make_unique<TextRectangle>(*activatableTButton.mpptr_activeTRect)),
                     mpptr_inactiveTRect(std::make_unique<TextRectangle>(*activatableTButton.mpptr_inactiveTRect)),
                     TextButton(activatableTButton), Rectangle(activatableTButton)
             {}
 
+            //Methods for changing mode
             ActivatableTButton& setActive();
             ActivatableTButton& setInactive();
             ActivatableTButton& toggleActive();
-            friend class ActivatableTButtonConstructor;
         };
 
     }
