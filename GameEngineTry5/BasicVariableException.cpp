d05c3d54c874ad64457c40f373495127a647d805
Abbta <67863193+Abbta@users.noreply.github.com>
2022-03-25 14:23:27 +0100

    basic variable exception cleaned

--- GameEngineTry5/BasicVariableException.cpp
+++ GameEngineTry5/BasicVariableException.cpp
@@ -1,14 +1,23 @@
 #include "BasicVariableException.h"
+#include "Constants.h"
 namespace EngineName
 {
 	namespace Exceptions
 	{
+		///Note: All specialisations follow <int> syntax and logic, the difference is typecasts and insertion techniques
+
+		/* BasicVariableException::std::string operator
+		*	returns explanation where '$' is replaced by the variable
+		*/
 		BasicVariableException<int>::operator std::string () const
 		{
 			std::string explanation = mpstr_errorExplanation;
+			//latest is to check that there is no '\' before the '$'
 			char latest = '0';
 			for(int i(0); i < explanation.size(); i++)
 			{
+				//iterate through the explanation and if there is a '$'
+					//without a '\' before it, replace it with the var
 				if (explanation[i] == '$' && latest != '\\')
 				{
 					explanation[i] = *mpptr_variable;
@@ -19,6 +28,7 @@ namespace EngineName
 			return explanation;
 		}
 
+		//same as <int> but explicitly casts to int
 		BasicVariableException<size_t>::operator std::string () const
 		{
 			std::string explanation = mpstr_errorExplanation;
@@ -35,12 +45,17 @@ namespace EngineName
 			return explanation;
 		}
 
+		//same as <int> but adds all characters one at a time
 		BasicVariableException<char*>::operator std::string () const
 		{
 			std::string explanation = mpstr_errorExplanation;
 			char latest = '0';
 			for (int i(0); i < explanation.size(); i++)
 			{
+				//if user enters '$' in the explanation, it would create an infinite loop and crash the program
+				//therefore check that it has not looped unreasonably many times
+				if (i >= Base::c_maxIterationCount)
+					throw Exceptions::BasicException("Infinite loop in variable exception, possibly variable identifier was used in the variable");
 				if (explanation[i] == '$' && latest != '\\')
 				{
 					explanation.insert(i, *mpptr_variable);
--- GameEngineTry5/Constants.h
+++ GameEngineTry5/Constants.h
@@ -11,6 +11,8 @@ namespace EngineName
 		const std::wstring c_mainWindowStandardTitle(L"EngineName Main Window"); //default: L"EngineName Main Window", name of app if dev supplies none
 		constexpr int c_windowCreationDefaultWidth(1000);
 		constexpr int c_windowCreationDefaultHeight(600);
+
+		constexpr int c_maxIterationCount(999); //default: 999, when there is a risk of infinite looping, the iteration variable is checked to not surpass this value
 	}
 
 	namespace Object
