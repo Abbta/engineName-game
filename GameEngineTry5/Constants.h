85c16699546b10a487f38579f370e20de9b90f47
Abbta <67863193+Abbta@users.noreply.github.com>
2022-04-19 11:38:34 +0200

    constants cleaned

--- GameEngineTry5/Constants.h
+++ GameEngineTry5/Constants.h
@@ -1,7 +1,6 @@
 #include "BaseIncludeLibraries.h"
 #include "Color.h"
 
-//contains constants for eng
 #pragma once
 namespace EngineName
 {
@@ -9,8 +8,8 @@ namespace EngineName
 	{
 		constexpr int c_mainWindowStandardFlags(WS_OVERLAPPEDWINDOW); //default: WS_OVERLAPPEDWINDOW, flags used when creating main window if dev supplies none
 		const std::wstring c_mainWindowStandardTitle(L"EngineName Main Window"); //default: L"EngineName Main Window", name of app if dev supplies none
-		constexpr int c_windowCreationDefaultWidth(1000);
-		constexpr int c_windowCreationDefaultHeight(600);
+		constexpr int c_windowCreationDefaultWidth(1000); //default: 1000, width of window when created in windowed mode
+		constexpr int c_windowCreationDefaultHeight(600); //default: 600, height of window when created in windowed mode
 
 		constexpr int c_maxIterationCount(999); //default: 999, when there is a risk of infinite looping, the iteration variable is checked to not surpass this value
 	}
