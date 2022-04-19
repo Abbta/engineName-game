0b42f3dc2e6f98cd967fd3a305743e8c18c9b1a1
Abbta <67863193+Abbta@users.noreply.github.com>
2022-04-19 07:24:03 +0200

    buttonconstructor optim

--- GameEngineTry5/ButtonConstructor.h
+++ GameEngineTry5/ButtonConstructor.h
@@ -6,13 +6,21 @@ namespace EngineName
 {
     namespace Object
     {
+        /*
+       * Button constructor class
+       * Used for constructing button
+       * Has .build()
+       *   constructs the object
+       *   gives back ref
+       * Stores all buttons within itself
+       */
         class ButtonConstructor :
             public ObjectConstructor
         {
         private:
             std::list<std::unique_ptr<Button>> mparr_allButtons;
         public:
-            ButtonConstructor(Base::ObjectContainer& world) : ObjectConstructor(world) {};
+            //button constructor is constructed in world which sends this as param
             ButtonConstructor(Base::ObjectContainer* world) : ObjectConstructor(*world) {};
 
             Button& build(const Button& button);
