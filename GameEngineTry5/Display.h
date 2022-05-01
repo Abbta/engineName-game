a7a3a3ae581f38554a8ebf0c7f4ba1f7db661bb2
Abbta <67863193+Abbta@users.noreply.github.com>
2022-05-01 23:04:15 +0200

    display cleaned

--- GameEngineTry5/Display.h
+++ GameEngineTry5/Display.h
@@ -10,12 +10,23 @@ namespace EngineName
     namespace Object
     {
         class Counter;
+
+        /*
+        * Display class
+        * Parent of displayImpl which handles construction
+        *   and display update
+        * 
+        * Is essentially a textrectangle with a counter associated
+        *   with it, accessed via mptr_
+        */
         class Display :
             public TextRectangle
         {
         protected:
+            //is actually counterImpl
             Counter* mptr_counter;
 
+            //counter signals that it has been changed
             virtual void mp_updateDisplay(const Counter& newCount)
             {
                 throw Exceptions::BasicException("Error, mp_updateDisplay accesed from Display base class");
