0ccb9bb969457254683f61a95ae233513d9bf5af
Abbta <67863193+Abbta@users.noreply.github.com>
2022-05-01 21:50:16 +0200

    countertasks cleaned

--- GameEngineTry5/CounterTasks.cpp
+++ GameEngineTry5/CounterTasks.cpp
@@ -3,6 +3,7 @@ namespace EngineName
 {
 	namespace Time
 	{
+		//assigns a new ref to mpptr_counter, used when constructing displayImpl
 		void UpdateDisplay::m_updateCounterPtr(Object::Counter& newCounter)
 		{
 			mpptr_counter = &newCounter;
--- GameEngineTry5/CounterTasks.h
+++ GameEngineTry5/CounterTasks.h
@@ -6,19 +6,28 @@ namespace EngineName
 {
 	namespace Time
 	{
+		/*
+		* AddToCounter task
+		* adds a specified amount to the specified counter
+		* is template
+		*/
 		template<class t_CountType>
 		class AddToCounter : public Task
 		{
 		protected:
+			//pointer to counter which in reality is CounterImpl of t_CountType type
 			Object::Counter* mpptr_counter;
 			t_CountType mp_amount;
 
 			virtual void mpf_perform(Base::ObjectContainer& world) override
 			{
+				//extract implemented part of mpptr_counter
 				auto counterImpl = dynamic_cast<Object::CounterImpl<t_CountType, UpdateDisplay>*>(mpptr_counter);
+				//note that the type of implemented part must match t_CountType in this class
 				if (counterImpl == nullptr)
 					throw Exceptions::BasicException("Type of AddToCounter doesn't match Counter");
 
+				//perform the addition using overloaded operator
 				*counterImpl += mp_amount;
 			}
 		public:
@@ -32,11 +41,19 @@ namespace EngineName
 		};
 
 
+		/*
+		* Uppdate display task
+		* updates the specified display to match the specified counter
+		* used primarily by a counter associated with a display to 
+		*	tell this display that count has been changed
+		* why this is done via a task is mainly for consistensy
+		*/
 		class UpdateDisplay : public Task
 		{
 		private:
 
 			Object::Display& mp_displayRef;
+			//note that counter is actually counterImpl
 			Object::Counter* mpptr_counter;
 
 			virtual void mpf_perform(Base::ObjectContainer& world) override
@@ -51,6 +68,7 @@ namespace EngineName
 
 			}
 
+			//assigns a new ref to mpptr_counter, used when constructing displayImpl
 			void m_updateCounterPtr(Object::Counter& newCounter);
 			friend class TaskContainer;
 		};
