9abae1a8323fb9acbd66aeb645bba0fdb2f64d7a
Abbta <67863193+Abbta@users.noreply.github.com>
2022-05-03 07:36:59 +0200

    objectgroup cleaned

--- GameEngineTry5/ObjectGroup.cpp
+++ GameEngineTry5/ObjectGroup.cpp
@@ -3,6 +3,7 @@ namespace EngineName
 {
 	namespace Object
 	{
+		//add an object to group
 		Object& ObjectGroup::add(Object& object)
 		{
 			mparr_objectPointers.push_back(&object);
--- GameEngineTry5/ObjectGroup.h
+++ GameEngineTry5/ObjectGroup.h
@@ -9,16 +9,19 @@ namespace EngineName
     namespace Object
     {
         /*
-        * (Used to group objects)
+        * ObjectGroup class
+        * object that represents a group of other objects
         */
         class ObjectGroup :
             public Object
         {
         protected:
+            //ptrs to all objects in group
             std::vector<Object*> mparr_objectPointers;
         public:
             ObjectGroup(Base::ObjectContainer& world) : Object(world) {};
 
+            //adds an object to the group
             Object& add(Object& object);
         };
     }
