4503a53dffd9728834077e318f291b6f34ef5e0a
Abbta <67863193+Abbta@users.noreply.github.com>
2022-06-01 19:43:07 +0200

    sceneconstructor cleaned

--- GameEngineTry5/SceneConstructor.cpp
+++ GameEngineTry5/SceneConstructor.cpp
@@ -3,8 +3,10 @@ namespace EngineName
 {
 	namespace Object
 	{
+		//builds a new empty scene
 		Scene& SceneConstructor::build(Base::ObjectContainer& world)
 		{
+			//construct dynamically and store
 			mparr_allScenes.push_back(std::make_unique<Scene>(Scene(world)));
 			return *mparr_allScenes.back();
 		}
--- GameEngineTry5/SceneConstructor.h
+++ GameEngineTry5/SceneConstructor.h
@@ -5,14 +5,24 @@ namespace EngineName
 {
     namespace Object
     {
+        /*
+        * sceneconstructor class
+        * used for constructing scenes
+        * has .build()
+        *   constructs the object
+        *   gives back ref
+        * stores all scenes within itself
+        */
         class SceneConstructor :
             public ObjectConstructor
         {
         private:
+            //stores all scenes
             std::list<std::unique_ptr<Scene>> mparr_allScenes;
         public:
             SceneConstructor(Base::ObjectContainer* world) : ObjectConstructor(world) {};
 
+            //constructs a new scene
             Scene& build(Base::ObjectContainer& world);
         };
     }
