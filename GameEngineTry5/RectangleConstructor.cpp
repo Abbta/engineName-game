d045776634457a70b292d60e8b8564600a6c5f2e
Abbta <67863193+Abbta@users.noreply.github.com>
2022-06-01 19:28:55 +0200

    rectangleconstructor cleaned

--- GameEngineTry5/RectangleConstructor.cpp
+++ GameEngineTry5/RectangleConstructor.cpp
@@ -6,15 +6,20 @@ namespace EngineName
 		//construct a rectangle, put it and refs to it in correct storages
 		Rectangle& RectangleConstructor::build(const Rectangle &rectangle)
 		{
+			//adds an empty spot in allvisibles, this signals that a new unique 
+				//visible is constructed meaning it needs a unique layerID
 			this->mf_makeRoomForNewVisible();
+			//dynamically allocate a new rectangle
 			this->mparr_allRectangles.push_back(std::make_unique<Rectangle>(rectangle));
+			//add rectangle to allVisibles
 			this->mf_registerNewVisible(*mparr_allRectangles.back());
+			//return ref to newly constructed rectangle
 			return *this->mparr_allRectangles.back();
 		}
 
 		//overload of above
 		Rectangle& RectangleConstructor::build(Base::ObjectContainer& world,
-			const Vector position, const int width, const int height, const Color color,
+			const Vector position, const td_width width, const td_height height, const Color color,
 			const unsigned int borderWidth, const Color borderColor)
 		{
 			this->mf_makeRoomForNewVisible();
--- GameEngineTry5/RectangleConstructor.h
+++ GameEngineTry5/RectangleConstructor.h
@@ -27,13 +27,13 @@ namespace EngineName
 
             Rectangle& build(const Rectangle &rectangle);
             Rectangle& build(Base::ObjectContainer& world,
-                const Vector position = &Vector(0, 0), const int width = 0, const int height = 0, const Color color = Color("000000"),
-                const unsigned int borderWidth = 0, const Color borderColor = Color("00000000")); //creates a rectangle
+                const Vector position = &Vector(0, 0), const td_width width = 0, const td_height height = 0, const Color color = Color("000000"),
+                const td_width borderWidth = 0, const Color borderColor = Color("00000000")); //creates a rectangle
             Rectangle& build(Base::ObjectContainer& world,
                 const Vector position = &Vector(0, 0), const double width = 0, const double height = 0, const Color color = Color("000000"),
-                const unsigned int borderWidth = 0, const Color borderColor = Color("00000000"))
+                const td_width borderWidth = 0, const Color borderColor = Color("00000000"))
             {
-                return build(world, position, static_cast<const int>(width), static_cast<const int>(height), color, borderWidth, borderColor);
+                return build(world, position, static_cast<const td_width>(width), static_cast<const td_height>(height), color, borderWidth, borderColor);
             }
         };
     }
