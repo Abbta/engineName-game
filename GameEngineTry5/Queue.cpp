d72b132762d02814e4541d4d7637bd1bfd2cc12e
Abbta <67863193+Abbta@users.noreply.github.com>
2022-06-01 07:39:54 +0200

    queue cleaned, also updated to v143
    visual studio 2022, very nice

--- GameEngineTry5/Queue.cpp
+++ GameEngineTry5/Queue.cpp
@@ -4,6 +4,7 @@ namespace EngineName
 {
 	namespace Time
 	{
+		//adds a task to the queue, sorted by how much time there is left, least time at back
 		void Queue::mpf_addToQueue(Task& task)
 		{
 			//create task and store it at the correct place
@@ -11,18 +12,23 @@ namespace EngineName
 			it = mparr_theQueue.begin();
 			do
 			{
+				//if the end is reached
 				if (it == mparr_theQueue.end())
 				{
+					//add task to back
 					mparr_theQueue.push_back(&task);
 					break;
 				}
+				//if we have iterated past the last element that has more time left than task
 				else if (task.timeLeft() > (*it)->timeLeft())
 				{
+					//insert task there
 					mparr_theQueue.insert(it, &task);
 					break; //we know where to insert it, no further need to keep iterating
 				}
 				else
 				{
+					//else keep iterating
 					it++;
 				}
 			} while (it != mparr_theQueue.end());
@@ -37,13 +43,21 @@ namespace EngineName
 		*/
 		void Queue::mpf_update(Base::ObjectContainer& world)
 		{
+			//if there is a queue
 			if (mparr_theQueue.size() > 0)
 			{
+				//to do list is a vector of all tasks that will be scheduled this update
 				std::vector<Task*> toDoList;
+
+				//keep track of time
 				const auto currentTime = std::chrono::system_clock::now();
-				const auto timeSinceLastCall = std::chrono::duration_cast<std::chrono::milliseconds>(mp_timeAtLastCall - currentTime);
+				const auto timeSinceLastCall = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - mp_timeAtLastCall);
 				mp_timeAtLastCall = currentTime;
-				while (!mparr_theQueue.back()->addTimeLeftInQueue(timeSinceLastCall))
+
+				//from the back of queue, check if element has negative time left
+					//when we have removed time since last call. If it has then remove it and 
+					//schedule it.  Break when an element still has time left or queue empties
+				while (!mparr_theQueue.back()->addTimeLeftInQueue(-timeSinceLastCall))
 				{
 					toDoList.push_back(mparr_theQueue.back());
 					mparr_theQueue.pop_back();
@@ -53,6 +67,17 @@ namespace EngineName
 						break;
 				}
 
+				//if there still are elements left in the queue, except for the one that broke that last loop
+					//remove time from them aswell, we can assume all of them end up with positive time since queue is sorted
+				if(mparr_theQueue.size() > 0)
+					//if statement needed as .size() will return an unsigned integer and - 1 on 0 will overflow
+					for (int i(0); i < mparr_theQueue.size() - 1; i++)
+					{
+						//note that the last element in queue is not uppdated since it was already updated in the last loop
+						mparr_theQueue[i]->addTimeLeftInQueue(-timeSinceLastCall);
+					}
+
+				//schedule all tasks that has been added to the to do list and remove them from it
 				std::vector<Task*>::const_iterator it;
 				it = toDoList.begin();
 				while(it != toDoList.end())
--- GameEngineTry5/Queue.h
+++ GameEngineTry5/Queue.h
@@ -7,8 +7,6 @@ namespace EngineName
 	namespace Action
 	{
 		class OnActionBase;
-		//temp:
-		class ActionListener;
 	}
 	namespace Base
 	{
@@ -24,6 +22,7 @@ namespace EngineName
 	{
 		/*
 		* Queue class
+		* handles execution of tasks that has been scheduled
 		* stores a ref to all task objects that has scheduled execution
 		* also keeps track of what system time it was last time it was called
 		*/
@@ -31,8 +30,11 @@ namespace EngineName
 		{
 		private:
 			std::chrono::time_point<std::chrono::system_clock> mp_timeAtLastCall;
-			TaskContainer mpc_taskContainer; //Just a bunch of std::list, should not need dynamic memory
+			//task container stores all tasks
+			TaskContainer mpc_taskContainer;
+			//vector containing ptrs to all tasks that are scheduled for execution at some time, sorted with least time left at back
 			std::vector<Task*> mparr_theQueue;
+
 			void mpf_addToQueue(Task& task);
 			void mpf_update(Base::ObjectContainer& world);
 		public:
