5a030f242679a860cd3d0c82699577d82b1d1265
Abbta <67863193+Abbta@users.noreply.github.com>
2022-06-27 15:09:47 +0200

    text format container cleaned

--- GameEngineTry5/TextFormatContainer.cpp
+++ GameEngineTry5/TextFormatContainer.cpp
@@ -4,17 +4,27 @@ namespace EngineName
 {
 	namespace Drawing
 	{
+		/*
+		* get font member function
+		* takes writefactory and a Font class as param
+		* returns winapi font associated with Font
+		*	either from already allocated inside map or allocates a new one and stores in map
+		*/
 		CComPtr<IDWriteTextFormat>& TextFormatContainer::getFont(CComPtr<IDWriteFactory>& writeFactory, const Font& font)
 		{
 			if(writeFactory == nullptr)
 				throw Exceptions::BasicException("writef factory is nullptr");
 
+			//get font from map
 			std::map<Font, CComPtr<IDWriteTextFormat>>::iterator formatPair = mp_formats.find(font);
 
+			//if font is not a part of map
 			if (formatPair == mp_formats.end())
 			{
+				//add "empty" font to map
 				std::map<Font, CComPtr<IDWriteTextFormat>>::iterator newFormatPair = mp_formats.emplace(font, CComPtr<IDWriteTextFormat>(nullptr)).first;
 			
+				//create a winapi font at that place
 				if(FAILED(writeFactory->CreateTextFormat(
 					font.mp_fontName.c_str(),
 					font.mpcxr_collection,
@@ -27,9 +37,11 @@ namespace EngineName
 				)))
 					throw Exceptions::BasicException("Failed creating textFormat");
 
+				//get the newly created font
 				formatPair = newFormatPair;
 			}
 
+			//return font
 			return formatPair->second;
 		}
 	}
--- GameEngineTry5/TextFormatContainer.h
+++ GameEngineTry5/TextFormatContainer.h
@@ -6,11 +6,18 @@ namespace EngineName
 {
 	namespace Drawing
 	{
+		/*
+		* Text format container class
+		* connects Font class to actual winapi fonts
+		* same as how Brushcontainer connects Color class to a brushes
+		*/
 		class TextFormatContainer
 		{
 		private:
+			//map containing all allocated fonts together with their associated Font classes
 			std::map <Font, CComPtr<IDWriteTextFormat>> mp_formats;
 		public:
+			//gets a certain font, allocates it if it does not already exist
 			CComPtr<IDWriteTextFormat>& getFont(CComPtr<IDWriteFactory>& writeFactory, const Font& font);
 		};
 	}
