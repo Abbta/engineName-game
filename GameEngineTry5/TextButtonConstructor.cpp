9778af3a3e1a7a913110f825324aeb36c92aa485
Abbta <67863193+Abbta@users.noreply.github.com>
2022-06-27 15:02:21 +0200

    textbuttonconstructor cleaned

--- GameEngineTry5/TextButtonConstructor.cpp
+++ GameEngineTry5/TextButtonConstructor.cpp
@@ -3,13 +3,21 @@ namespace EngineName
 {
 	namespace Object
 	{
-		//construct a Button, put it and refs to it in correct storages
+		//allocates memory, constructs and stores a button in world
+		//returns ref to newly constructed
 		TextButton& TextButtonConstructor::build(const TextButton& textButton)
 		{
+			//adds an empty spot in allvisibles, this signals that a new unique 
+				//visible is constructed meaning it needs a unique layerID
 			this->mf_makeRoomForNewVisible();
+			//construct the button dynamically
 			this->mparr_allTextButtons.push_back(std::make_unique<TextButton>(textButton));
+			//since on click area is a ref to button itself it needs to be assigned after button
+				//has been constructed
 			this->mparr_allTextButtons.back()->mpf_changeOnClickArea(*mparr_allTextButtons.back());
+			//add button to allVisibles 
 			this->mf_registerNewVisible(*mparr_allTextButtons.back());
+			//return ref to button
 			return *this->mparr_allTextButtons.back();
 		}
 	}
--- GameEngineTry5/TextButtonConstructor.h
+++ GameEngineTry5/TextButtonConstructor.h
@@ -5,6 +5,14 @@ namespace EngineName
 {
     namespace Object
     {
+        /*
+        * textbuttonconstructor class
+        * used for constructing textbuttons
+        * has .build()
+        *   constructs the object
+        *   gives back ref
+        * stores all textbuttons within itself
+        */
         class TextButtonConstructor :
             public ObjectConstructor
         {
