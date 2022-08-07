3e160bb6e3393699ba58ae097adc5d4a5c1af0c0
Abbta <67863193+Abbta@users.noreply.github.com>
2022-08-07 19:50:56 +0200

    width and height cleaned

--- GameEngineTry5/WidthAndHeight.cpp
+++ GameEngineTry5/WidthAndHeight.cpp
@@ -6,43 +6,40 @@ namespace EngineName
 		Width::Width(MainWindow& mainWindow) :
 			mp_mainWinRef(mainWindow)
 		{
+			//start with assigning width of the actual window as width, meaning all kinds of widths are the same
 			mp_width = c_windowCreationDefaultWidth;
 		}
 
-		Width& Width::operator=(const unsigned int newWidth)
+		//change width
+		//TODO: update other widths when this changes, implent this when more type of widths are implemented
+		Width& Width::operator=(const td_width newWidth)
 		{
 			mp_width = newWidth;
 			//recalculate
 			return *this;
 		}
 
-		const FLOAT Width::toOutputWidth(const unsigned int width) const
-		{
-			RECT temp;
-			GetWindowRect(mp_mainWinRef.Window(), &temp);
-			return static_cast<FLOAT>(width / static_cast<double>(mp_width)) * (temp.right - temp.left);
-		}
-
-		const FLOAT Width::toOutputWidth(const signed int width) const
-		{
-			RECT temp;
-			GetWindowRect(mp_mainWinRef.Window(), &temp);
-			return static_cast<FLOAT>(width / static_cast<double>(mp_width)) * (temp.right - temp.left);
-		}
+		//transform to winapi width, double overload as this causes no additional casts
 		const FLOAT Width::toOutputWidth(const double width) const
 		{
+			//get rect of window
 			RECT temp;
 			GetWindowRect(mp_mainWinRef.Window(), &temp);
+			//(myWidth / width of output) = how much of the output area that is to be occupied by this width
+			//this times the pixel width of the window gets how many pixels myWidth represents
 			return static_cast<FLOAT>(width / mp_width) * (temp.right - temp.left);
 		}
 
-		const unsigned int Width::fromInputWidth(const short inputWidth) const
+		//transform from winapi width
+		const td_width Width::fromInputWidth(const FLOAT inputWidth) const
 		{
+			//inverse of toOutputWidth()
 			RECT temp;
 			GetWindowRect(mp_mainWinRef.Window(), &temp);
-			return static_cast<unsigned int>((inputWidth / (static_cast<double>(temp.right) - static_cast<double>(temp.left))) * mp_width);
+			return static_cast<td_width>((inputWidth / (static_cast<double>(temp.right) - static_cast<double>(temp.left))) * mp_width);
 		}
 
+		//same but for height
 
 		Height::Height(MainWindow& mainWindow)
 			:mp_mainWinRef(mainWindow)
@@ -50,25 +47,18 @@ namespace EngineName
 			mp_height = c_windowCreationDefaultHeight;
 		}
 
-		Height& Height::operator=(const unsigned int newHeight)
+		Height& Height::operator=(const td_height newHeight)
 		{
 			mp_height = newHeight;
 			//recalculate
 			return *this;
 		}
 
-		const FLOAT Height::toOutputHeight(const unsigned int height) const
+		const FLOAT Height::toOutputHeight(const double height) const
 		{
 			RECT temp;
 			GetWindowRect(mp_mainWinRef.Window(), &temp);
-			return static_cast<FLOAT>(height / static_cast<double>(mp_height)) * (temp.bottom - temp.top);
-		}
-
-		const FLOAT Height::toOutputHeight(const signed int height) const
-		{
-			RECT temp;
-			GetWindowRect(mp_mainWinRef.Window(), &temp);
-			return static_cast<FLOAT>(height / static_cast<double>(mp_height)) * (temp.bottom - temp.top);
+			return static_cast<FLOAT>(height / mp_height) * (temp.bottom - temp.top);
 		}
 
 		const unsigned int Height::fromInputHeight(const short inputHeight) const
--- GameEngineTry5/WidthAndHeight.h
+++ GameEngineTry5/WidthAndHeight.h
@@ -4,35 +4,52 @@ namespace EngineName
 {
 	namespace Base
 	{
+		/*
+		* Width and Height classes
+		* bridge between coordinates inside engine to the output coordinates sent to winapi
+		* (coordinates sent to winapi is always pixels in the mainwindow, while coordinates inside is not necessarily that)
+		* contains width/height of output area from engine's perspective
+		* contains ref to mainwindow, gives access to width/height of the actual window from windows' perspective
+		* contains functions for transforming an arbitrary width inside engine to width pixels in window
+		* 
+		* TODO: make classes support different kinds of lengths, for example those who don't change when windowsize changes
+		*/
 		class Width
 		{
 		private:
-			//TODO: windows doesent allow widths above short, change int to short
-			unsigned int mp_width;
+			//width for reference to constant width lengths
+			td_width mp_width;
+
+			//ref to mainwindow
 			MainWindow& mp_mainWinRef;
 		public:
 			Width(MainWindow& mainWindow);
-
-			operator const unsigned int() const { return mp_width; }
-			Width& operator=(const unsigned int newWidth);
-			const FLOAT toOutputWidth(const unsigned int width) const;
-			const FLOAT toOutputWidth(const signed int width) const;
+			//get width of output
+			operator const td_width() const { return mp_width; }
+			//assign width of output
+			Width& operator=(const td_width newWidth);
+			//transform to winapi width (pixels in window)
+			const FLOAT toOutputWidth(const td_width width) const {return toOutputWidth(static_cast<double>(width));}
+			const FLOAT toOutputWidth(const signed int width) const { return toOutputWidth(static_cast<double>(width)); }
 			const FLOAT toOutputWidth(const double width) const;
-			const unsigned int fromInputWidth(const short inputWidth) const;
+			//transform from winapi width
+			const unsigned int fromInputWidth(const FLOAT inputWidth) const;
 		};
 
+		//exactly the same as width
 		class Height
 		{
 		private:
-			unsigned int mp_height;
+			td_height mp_height;
 			MainWindow& mp_mainWinRef;
 		public:
 			Height(MainWindow& mainWindow);
 
-			operator const unsigned int() const { return mp_height; }
-			Height& operator=(const unsigned int newHeight);
-			const FLOAT toOutputHeight(const unsigned int height) const;
-			const FLOAT toOutputHeight(const signed int height) const;
+			operator const td_height() const { return mp_height; }
+			Height& operator=(const td_height newHeight);
+			const FLOAT toOutputHeight(const td_height height) const { return toOutputHeight(static_cast<double>(height)); }
+			const FLOAT toOutputHeight(const signed int height) const { return toOutputHeight(static_cast<double>(height)); }
+			const FLOAT toOutputHeight(const double height) const;
 			const unsigned int fromInputHeight(const short inputHeight) const;
 		};
 	}
