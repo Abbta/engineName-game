711452456976b9aee268a07cfacc5ff53da07b0d
Abbta <67863193+Abbta@users.noreply.github.com>
2022-08-07 17:10:48 +0200

    typedefs cleaned

--- GameEngineTry5/Typedefs.h
+++ GameEngineTry5/Typedefs.h
@@ -1,12 +1,12 @@
 #pragma once
 namespace EngineName
 {
-	typedef unsigned int td_width;
-	typedef unsigned int td_height;
-	typedef double td_radius;
-	typedef double td_angle;
-	typedef int td_xDirection;
-	typedef int td_yDirection;
-	typedef size_t td_ID;
+	typedef unsigned int td_width; //width of visibles inside engine
+	typedef unsigned int td_height; //height of visible inside engine
+	typedef double td_radius; //calculating width and height from a distance
+	typedef double td_angle; //calculating widht and height from an angle
+	typedef int td_xDirection; //relative position in x direction
+	typedef int td_yDirection; //relative position in y direction
+	typedef size_t td_ID; //id of objects
 	typedef int td_scheduleTime; //storing how many ms a task will be scheduled with
 }
